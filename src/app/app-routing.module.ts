f4fb26f722365723f65d4c57de91cef8dfb94502
Sebastian <jsruiz7411@misena.edu.co>
2021-03-13 12:26:48 -0500

    rutas creadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo: '/ingresarpresupuesto', pathMatch: 'full'},
+  {path:"ingresarpresupuesto",component: IngresarPresupuestoComponent},
+  {path:"gastos",component: GastosComponent},
+  {path:"**", redirectTo: '/ingresarpresupuesto', pathMatch: 'full' }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
