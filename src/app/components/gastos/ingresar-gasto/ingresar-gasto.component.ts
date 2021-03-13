0cb51d79910a516ff1bd80e6caa9277adca23fbc
Sebastian <jsruiz7411@misena.edu.co>
2021-03-13 15:45:26 -0500

    Listo

--- src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresarGastoComponent implements OnInit {
 
-  constructor() { }
+  nombreGasto: string;
+  cantidad: number;
+  formularioIncorrecto: boolean;
+   textIncorrecto: string;
+
+  constructor() {
+    this.nombreGasto = '',
+    this.cantidad = 0;
+    this.formularioIncorrecto= false;
+    this.textIncorrecto = 'Nombre gasto o cantidad incorrecta'
+   }
 
   ngOnInit(): void {
+  }
+
+  agregarGasto(){
+    if(this.nombreGasto === ''  || this.cantidad <= 0){
+      this.formularioIncorrecto = true
+
+    }
+
+    else {
+      this.formularioIncorrecto = false;
+      this.nombreGasto = '';
+      this.cantidad = 0;
+    }
   }
 
 }
